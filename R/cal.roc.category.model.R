ef19db008366bc372a4b0b9ce9df9c06dbf8908e
zwxiangya <95095098+zwxiangya@users.noreply.github.com>
2023-11-10 14:55:40 +0800

    Update cal.roc.category.model.R

--- R/cal.roc.category.model.R
+++ R/cal.roc.category.model.R
@@ -1,9 +1,9 @@
-#' Title
+#' Calculating the receiver operating characteristic after developing the category predictive model
 #'
-#' @param res.by.ML.Dev.Pred.Category.Sig
-#' @param cohort.for.cal
+#' @param res.by.ML.Dev.Pred.Category.Sig    Output of function ML.Dev.Pred.Category.Sig
+#' @param cohort.for.cal  A data frame with the 'ID' and 'Var' as the first two columns. Starting in the fourth column are the variables that contain variables of the model you want to build. The second column 'Var' only contains 'Y' or 'N'.
 #'
-#' @return
+#' @return  A list containing the receiver operating characteristic of each predictive model.
 #' @export
 #'
 #' @examples
